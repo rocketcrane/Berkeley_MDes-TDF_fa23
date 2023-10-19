26d73e409e0ddf417764a7f171bdba417b75da17
Lingxiu Zhang <47123365+rocketcrane@users.noreply.github.com>
2023-10-18 17:55:42 -0700

    delete useless

--- ble-logging/target/4.2.0/argon/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-/Users/lingxiu/Documents/0-documents/2023-2024-MDes/TDF/tdf-fa23-rocketcrane/ble-logging/target/4.2.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
